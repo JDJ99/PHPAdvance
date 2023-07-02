4d816ca2a4c2012517eb12ba7adb2b7d06f49327
JDJ99 <jermainjungerman@gmail.com>
2023-07-02 17:14:14 +0200

    update

--- read.php
+++ read.php
@@ -1,30 +1,30 @@
 <?php
-
-require_once 'vendor/autoload.php';
-
-use App\Database\Database;
-use App\Config\DatabaseConfig;
-use App\Repositories\Repository;
-
-$database = new Database(
-    DatabaseConfig::HOST,
-    DatabaseConfig::USERNAME,
-    DatabaseConfig::PASSWORD,
-    DatabaseConfig::DBNAME
-);
-$repository = new Repository($database);
-
-$posts = $repository->findAll();
-
-if (!empty($posts)) {
-    foreach ($posts as $post) {
-        echo '<div class="post">
-                <h3>' . $post['title'] . '</h3>
-                <p>' . $post['content'] . '</p>
-                <p class="timestamp">Posted on ' . $post['created_at'] . '</p>
-              </div>';
-    }
-} else {
-    echo '<p>No blog posts found.</p>';
-}
-?>
+//
+//require_once 'vendor/autoload.php';
+//
+//use App\Database\Database;
+//use App\Config\DatabaseConfig;
+//use App\Repositories\Repository;
+//
+//$database = new Database(
+//    DatabaseConfig::HOST,
+//    DatabaseConfig::USERNAME,
+//    DatabaseConfig::PASSWORD,
+//    DatabaseConfig::DBNAME
+//);
+//$repository = new Repository($database);
+//
+//$posts = $repository->findAll();
+//
+//if (!empty($posts)) {
+//    foreach ($posts as $post) {
+//        echo '<div class="post">
+//                <h3>' . $post['title'] . '</h3>
+//                <p>' . $post['content'] . '</p>
+//                <p class="timestamp">Posted on ' . $post['created_at'] . '</p>
+//              </div>';
+//    }
+//} else {
+//    echo '<p>No blog posts found.</p>';
+//}
+//?>
